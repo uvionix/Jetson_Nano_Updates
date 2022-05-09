e571321cc64c30345e5574adbb043f9a4c58026e
sdarmonski <102170249+sdarmonski@users.noreply.github.com>
2022-05-09 13:17:38 +0300

    Fixed mode switch bug when herelink airunit is connected

---
+++ patch-001-09-May-2022.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "*** Applying patch-001-09-May-2022 - Fixed mode switch bug when herelink airunit is connected ***"
+
+echo "Stopping network-watchdog service..."
+service network-watchdog stop
+
+echo "Stopping VPN service..."
+service openvpn-autostart stop
+
+echo "Stopping MAVProxy..."
+service mavproxy-autostart stop
+
+# Get the logged-in username
+usr=$(logname)
+
+# Create the download directory
+mkdir /home/$usr/Repos
+cd /home/$usr/Repos
+
+# Clone the new repositories
+echo "Cloning repositories..."
+repoexists=$(ls | grep "Jetson_Nano_Python_Scripts")
+
+if [ ! -z "$repoexists" ]
+then
+	rm -d -r Jetson_Nano_Python_Scripts
+fi
+
+git clone https://github.com/sdarmonski/Jetson_Nano_Python_Scripts.git
+
+# Copy the new files over the old ones
+echo "Copying new files..."
+cp /home/$usr/Repos/Jetson_Nano_Python_Scripts/chmod_offline.py /usr/local/bin/
+
+chmod +x /usr/local/bin/chmod_offline.py
+
+# Delete created repositories
+echo "Removing downloaded repositories..."
+rm -d -r Jetson_Nano_Python_Scripts
+
+echo "Starting network-watchdog service..."
+service network-watchdog start
+
+echo "Update successful!"
