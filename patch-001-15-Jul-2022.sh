ad4d1bbccb745d3796f60fa9a127fcdb5a956235
sdarmonski <s.darmonski@uvionix.com>
2022-07-15 12:50:31 +0300

    Camera record, stream and display using gstreamer

---
+++ patch-001-15-Jul-2022.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "*** Applying patch-001-15-Jul-2022 - Camera record, stream and display using gstreamer ***"
+
+# Get the logged-in username
+usr=$(logname)
+
+# Create the download directory
+mkdir /home/$usr/Repos
+cd /home/$usr/Repos
+
+# Clone the new repositories
+echo "Cloning repositories..."
+repoexists=$(ls | grep "Jetson_Nano_Linux_Services")
+
+if [ ! -z "$repoexists" ]
+then
+	rm -d -r Jetson_Nano_Linux_Services
+fi
+
+repoexists=$(ls | grep "Jetson_Nano_Shell_Scripts")
+
+if [ ! -z "$repoexists" ]
+then
+	rm -d -r Jetson_Nano_Shell_Scripts
+fi
+
+git clone https://github.com/sdarmonski/Jetson_Nano_Linux_Services.git
+git clone https://github.com/sdarmonski/Jetson_Nano_Shell_Scripts.git
+
+# Copy the new files over the old ones
+echo "Copying new files..."
+cp /home/$usr/Repos/Jetson_Nano_Linux_Services/gstreamer-autostart.service /etc/systemd/system/
+cp /home/$usr/Repos/Jetson_Nano_Linux_Services/gstreamer-setup /etc/default/
+cp /home/$usr/Repos/Jetson_Nano_Shell_Scripts/gstreamer-start.sh /usr/local/bin/
+cp /home/$usr/Repos/Jetson_Nano_Shell_Scripts/network-watchdog.sh /usr/local/bin/
+
+echo "Modify file permissions..."
+chmod +x /usr/local/bin/gstreamer-start.sh
+chmod +x /usr/local/bin/network-watchdog.sh
+
+echo "Reload daemons..."
+systemctl daemon-reload
+
+# Delete created repositories
+echo "Removing downloaded repositories..."
+rm -d -r Jetson_Nano_Linux_Services
+rm -d -r Jetson_Nano_Shell_Scripts
+
+echo "Update successful!"
