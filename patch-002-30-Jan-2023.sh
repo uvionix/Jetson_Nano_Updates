14249af30064a4a8ee1980c6f4a86d1ebe79c1cc
xoss <s.darmonski@uvionix.com>
2023-01-30 18:11:58 +0200

    Fixed initialization bug - vehicle armed status is now initially read from file

---
+++ patch-002-30-Jan-2023.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "*** Applying patch-002-30-Jan-2023 - Fixed initialization bug - vehicle armed status is now initially read from file ***"
+
+# Get the logged-in username
+usr=$(logname)
+
+# Create the download directory
+mkdir -p /home/$usr/Repos
+cd /home/$usr/Repos
+
+# Clone the new repositories
+repoexists=$(ls | grep "Jetson_Nano_Python_Scripts")
+
+if [ ! -z "$repoexists" ]
+then
+	rm -d -r Jetson_Nano_Python_Scripts
+fi
+
+git clone https://github.com/sdarmonski/Jetson_Nano_Python_Scripts.git
+
+# Copy the new files over the old ones
+echo "Copying new files..."
+cp /home/$usr/Repos/Jetson_Nano_Python_Scripts/update-uav-status.py /usr/local/bin/
+
+echo "Modify file permissions..."
+chmod +x /usr/local/bin/update-uav-status.py
+
+echo "Reload daemons..."
+systemctl daemon-reload
+
+# Delete created repositories
+echo "Removing downloaded repositories..."
+rm -d -r Jetson_Nano_Python_Scripts
+
+echo "Update successful!"
